656ee80ef0667d0aa59c977ce9c0020a2101068b
root <aobersnel@apnex.com.au>
2021-06-22 12:43:12 +1000

    Updates..

--- phase0-deploy/module-avi-controller/main.tf
+++ phase0-deploy/module-avi-controller/main.tf
@@ -67,7 +67,6 @@ resource "vsphere_virtual_machine" "vm" {
 
 resource "null_resource" "healthcheck" {
 	triggers = {
-		always_run	= timestamp()
 		avi_addresses	= vsphere_virtual_machine.vm.guest_ip_addresses[0]
 		avi-endpoint	= "avic.lab01.one"
 	}
